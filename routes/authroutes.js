ec0abf000c0b293cc139a7ac9a4df16f33e38150
Piretz <94498274+Piretz@users.noreply.github.com>
2025-07-08 02:50:46 -1200

    update

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js'; // Add `.js` if it's also an ES module
+import User from '../models/user.js'; // Add `.js` if it's also an ES module
 
 const router = Router();
 
